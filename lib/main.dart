234db649b674aec13088bb24d1ef6cea4170d8fd
Sirius-13 <cwxdesmond2002@gmail.com>
2023-05-10 01:19:14 +0800

    Changed Title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Music Player 1'),
+      home: const MyHomePage(title: 'Music Player'),
     );
   }
 }
